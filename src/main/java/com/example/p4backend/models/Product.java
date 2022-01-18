605711bc10f70e3e60914a5278943e49e46fdd56
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-01-18 14:13:57 +0100

    Added: Product model

---
+++ src/main/java/com/example/p4backend/models/Product.java
@@ -0,0 +1,24 @@
+package com.example.p4backend.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.Decimal128;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "products")
+public class Product {
+    private String id;
+    private String name;
+    private Decimal128 cost;
+    private String actionId;
+
+    public Product(String name, Decimal128 cost, String actionId) {
+        this.name = name;
+        this.cost = cost;
+        this.actionId = actionId;
+    }
+}
