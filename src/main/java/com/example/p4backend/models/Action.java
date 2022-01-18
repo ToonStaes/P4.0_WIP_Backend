1ce35598813a9bf3e859cbf3a1ec8eed924f3aa7
Toon Staes <r0784094@student.thomasmore.be>
2022-01-18 15:38:04 +0100

    added start and end date to an action

--- src/main/java/com/example/p4backend/models/Action.java
+++ src/main/java/com/example/p4backend/models/Action.java
@@ -8,6 +8,8 @@ import org.bson.types.Decimal128;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,11 +22,15 @@ public class Action {
     private Decimal128 goal;
     private String description;
     private String vzwID;
+    private Date startDate;
+    private Date endDate;
 
-    public Action(String name, @Nullable Decimal128 goal, String description, String vzwID) {
+    public Action(String name, @Nullable Decimal128 goal, String description, String vzwID, Date endDate) {
         this.name = name;
         this.goal = goal;
         this.description = description;
         this.vzwID = vzwID;
+        this.endDate = endDate;
+        this.startDate = new Date();
     }
 }
