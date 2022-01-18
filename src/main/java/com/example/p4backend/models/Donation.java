36c0df0e78d43f4db5d8512de9580bf91e94f241
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-01-18 14:30:20 +0100

    Added: Donation model

---
+++ src/main/java/com/example/p4backend/models/Donation.java
@@ -0,0 +1,26 @@
+package com.example.p4backend.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.Decimal128;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "donations")
+public class Donation {
+    @Id
+    private String id;
+    private String userId;
+    private String vzwId;
+    private Decimal128 amount;
+
+    public Donation(String userId, String vzwId, Decimal128 amount) {
+        this.userId = userId;
+        this.vzwId = vzwId;
+        this.amount = amount;
+    }
+}
