57f19f1d21b80f27f39049aaf50d3021fdfe9ccc
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-01-19 11:25:07 +0100

    Added: PostConstruct

--- src/main/java/com/example/p4backend/controllers/VzwController.java
+++ src/main/java/com/example/p4backend/controllers/VzwController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +17,52 @@ public class VzwController {
 
     @Autowired
     private VzwRepository vzwRepository;
+
+    @PostConstruct
+    public void fillDB() {
+        if (vzwRepository.count() == 0) {
+            Vzw vzw1 = new Vzw(
+                    "vzw1",
+                    "vzw1.kasterlee@mail.com",
+                    "be1234566798",
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptas sequi voluptatum pariatur! Quae cumque quidem dolor maxime enim debitis omnis nemo facilis sequi autem? Quae tenetur, repellat vero deleniti vitae dolores? Cum tempore, mollitia provident placeat fugit earum, sint, quae iusto optio ea officiis consectetur sit necessitatibus itaque explicabo?",
+                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+                    "https://http.cat/200",
+                    "password");
+
+            Vzw vzw2 = new Vzw(
+                    "vzw2",
+                    "vzw2.malle@mail.com",
+                    "be1234566798",
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptas sequi voluptatum pariatur! Quae cumque quidem dolor maxime enim debitis omnis nemo facilis sequi autem? Quae tenetur, repellat vero deleniti vitae dolores? Cum tempore, mollitia provident placeat fugit earum, sint, quae iusto optio ea officiis consectetur sit necessitatibus itaque explicabo?",
+                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+                    "https://http.cat/201",
+                    "password");
+
+            Vzw vzw3 = new Vzw(
+                    "vzw3",
+                    "vzw3.herselt@mail.com",
+                    "be1234599798",
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptas sequi voluptatum pariatur! Quae cumque quidem dolor maxime enim debitis omnis nemo facilis sequi autem? Quae tenetur, repellat vero deleniti vitae dolores? Cum tempore, mollitia provident placeat fugit earum, sint, quae iusto optio ea officiis consectetur sit necessitatibus itaque explicabo?",
+                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+                    "https://http.cat/202",
+                    "password");
+
+            Vzw vzw4 = new Vzw(
+                    "vzw4",
+                    "vzw4.malle@mail.com",
+                    "be1234566798",
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptas sequi voluptatum pariatur! Quae cumque quidem dolor maxime enim debitis omnis nemo facilis sequi autem? Quae tenetur, repellat vero deleniti vitae dolores? Cum tempore, mollitia provident placeat fugit earum, sint, quae iusto optio ea officiis consectetur sit necessitatibus itaque explicabo?",
+                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+                    "https://http.cat/203",
+                    "password");
+
+            vzwRepository.save(vzw1);
+            vzwRepository.save(vzw2);
+            vzwRepository.save(vzw3);
+            vzwRepository.save(vzw4);
+        }
+    }
 
     @GetMapping("/vzws")
     public List<Vzw> getAll() {
