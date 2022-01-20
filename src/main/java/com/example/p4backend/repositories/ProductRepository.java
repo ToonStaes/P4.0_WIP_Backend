ca33f672d436a9f23a3a424851d6943b4bf70d35
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-01-20 16:18:32 +0100

    Added: parameter to request an action uncluding it's goal progress percentage

--- src/main/java/com/example/p4backend/repositories/ProductRepository.java
+++ src/main/java/com/example/p4backend/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.example.p4backend.repositories;
 import com.example.p4backend.models.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findProductsByActionId(String actionID);
 }
