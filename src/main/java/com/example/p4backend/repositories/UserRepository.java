86965b1a8dd870e167d3b22669664c6204d80205
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-01-27 11:06:18 +0100

    Added: getUserByEmail and Unit Tests

--- src/main/java/com/example/p4backend/repositories/UserRepository.java
+++ src/main/java/com/example/p4backend/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.p4backend.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository <User, String> {
+    Optional<User> findFirstByEmail(String email);
 }
