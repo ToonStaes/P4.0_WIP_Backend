f8f7e7637edc811d8df01bd7360c4aeb5a290e6f
Toon Staes <r0784094@student.thomasmore.be>
2022-02-01 10:38:40 +0100

    Unit test

--- src/main/java/com/example/p4backend/models/DTOs/ProductDTO.java
+++ src/main/java/com/example/p4backend/models/DTOs/ProductDTO.java
@@ -1,9 +1,11 @@
 package com.example.p4backend.models.DTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bson.types.Decimal128;
 
 @Getter
+@AllArgsConstructor
 public class ProductDTO {
     private String name;
     private Decimal128 cost;
