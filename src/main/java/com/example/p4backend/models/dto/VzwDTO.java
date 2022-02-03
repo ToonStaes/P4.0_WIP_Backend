0ed8fe1cf97d21122ac8787732d6531c2b04067b
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-03 16:04:35 +0100

    Removed: VzwDTO lombok toString

--- src/main/java/com/example/p4backend/models/dto/VzwDTO.java
+++ src/main/java/com/example/p4backend/models/dto/VzwDTO.java
@@ -2,11 +2,9 @@ package com.example.p4backend.models.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class VzwDTO {
     // VZW
     private String name;
